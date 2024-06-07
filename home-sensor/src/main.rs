334b1cfff5982285e14888427e3872fdfd9e4663
PetoMPP <piotreq22@gmail.com>
2024-06-07 10:29:11 +0200

    make basic nicer

--- home-sensor/src/main.rs
+++ home-sensor/src/main.rs
@@ -6,7 +6,6 @@ use critical_section::Mutex;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    delay::Delay,
     gpio::{Event, Gpio6, Gpio7, Input, Io, Level, Output, Pull},
     peripherals::Peripherals,
     prelude::*,
@@ -21,32 +20,24 @@ static LED: Mutex<RefCell<Option<Output<Gpio7>>>> = Mutex::new(RefCell::new(None
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let _clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    println!("Hello world!");
+    println!("Starting..");
 
     let mut io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     // Set the interrupt handler for GPIO interrupts.
     io.set_interrupt_handler(handler);
-    // Set GPIO7 as an output, and set its state high initially.
-    let mut led = Output::new(io.pins.gpio7, Level::Low);
-
-    // Set GPIO6 as an input
+    let led = Output::new(io.pins.gpio7, Level::Low);
     let mut button = Input::new(io.pins.gpio6, Pull::Up);
 
-    // ANCHOR: critical_section
     critical_section::with(|cs| {
+        // Assign handler and RefCells with the GPIO instances.
         button.listen(Event::AnyEdge);
         BUTTON.borrow_ref_mut(cs).replace(button);
         LED.borrow_ref_mut(cs).replace(led);
     });
-    // ANCHOR_END: critical_section
 
-    // let delay = Delay::new(&clocks);
-    loop {
-        // led.toggle();
-        // delay.delay_millis(500u32);
-    }
+    loop {}
 }
 
 #[handler]
@@ -54,6 +45,7 @@ fn handler() {
     critical_section::with(|cs| {
         let mut but = BUTTON.borrow_ref_mut(cs);
         let but = but.as_mut().unwrap();
+        // Set the LED level to the opposite of the button level.
         LED.borrow_ref_mut(cs)
             .as_mut()
             .unwrap()
