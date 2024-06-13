c0e1dbdcc91c2a51b46f92eafcdd098f8e7e0082
PetoMPP <piotreq22@gmail.com>
2024-06-13 20:24:25 +0200

    add error inner for htmx

--- home-api/src/website/mod.rs
+++ home-api/src/website/mod.rs
@@ -1,5 +1,5 @@
 use askama::Template;
-use axum::{http::StatusCode, response::Html};
+use axum::{http::{HeaderMap, StatusCode}, response::Html};
 
 pub mod home;
 pub mod scanner;
@@ -11,13 +11,30 @@ pub struct ErrorTemplate {
     pub message: String,
 }
 
-pub async fn not_found() -> Html<String> {
-    Html(
-        ErrorTemplate {
-            status: StatusCode::NOT_FOUND,
-            message: "Not Found".to_string(),
-        }
-        .render()
-        .unwrap(),
-    )
+#[derive(Template)]
+#[template(path = "pages/error-inner.html")]
+pub struct ErrorInnerTemplate {
+    pub status: StatusCode,
+    pub message: String,
+}
+
+pub async fn not_found(headers: HeaderMap) -> Html<String> {
+    match headers.contains_key("Hx-Request") {
+        true => Html(
+            ErrorInnerTemplate {
+                status: StatusCode::NOT_FOUND,
+                message: "Not Found".to_string(),
+            }
+            .render()
+            .unwrap(),
+        ),
+        false => Html(
+            ErrorTemplate {
+                status: StatusCode::NOT_FOUND,
+                message: "Not Found".to_string(),
+            }
+            .render()
+            .unwrap(),
+        ),
+    }
 }
