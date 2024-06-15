b3e4b2fea3703e3c1e65cd3e929cabd6f9d13efb
PetoMPP <piotreq22@gmail.com>
2024-06-15 16:21:46 +0200

    sensor auth improvement, expose sensor data

--- home-sensor/src/http/route/mod.rs
+++ home-sensor/src/http/route/mod.rs
@@ -8,13 +8,11 @@ mod sensor;
 #[derive(Debug)]
 pub struct Route {
     pub is_match: fn(&Request) -> bool,
-    pub response: fn(&Request) -> Response,
+    pub response: fn(&Request, bool) -> Response,
 }
 
 pub fn routes() -> Vec<Route, 16> {
     let mut routes = Vec::new();
-    routes.push(pair::pair()).unwrap();
-    routes.push(pair::confirm()).unwrap();
     routes.push(sensor::get()).unwrap();
     routes.push(sensor::post()).unwrap();
     routes
