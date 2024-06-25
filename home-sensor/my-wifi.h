27cad370b339f17f70f4bce219cc6b2b652f4a6b
PetoMPP <piotreq22@gmail.com>
2024-06-25 20:15:46 +0200

    create arduino project with wifi connect and temp sensor

---
+++ home-sensor/my-wifi.h
@@ -0,0 +1,33 @@
+#include "WiFi.h"
+
+
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void connect_wifi(char ssid[], char pass[]) {
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+
+  WiFi.useStaticBuffers(true);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
+}
