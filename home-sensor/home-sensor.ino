54b5e3ce7dbe393e94fd30b837c71cd3ab4846c0
PetoMPP <piotreq22@gmail.com>
2024-06-27 17:37:43 +0200

    sensor: add basic http server and request parser while performing temp readings

--- home-sensor/home-sensor.ino
+++ home-sensor/home-sensor.ino
@@ -1,19 +1,27 @@
 #include "my-dht.h"
 #include "my-wifi.h"
+#include "my-server.h"
 #include "secret.h"
+
+ulong dht_timeout_ms = 20000;
+ulong next_dht_read = 0;
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
 
   connect_wifi(ssid, pass);
-
   dht_init();
+  server_init();
+  next_dht_read = millis() + dht_timeout_ms;
 }
 
 void loop() {
-  delay(2000);
-  struct DhtMeasurement measurement = read_dht_measurement();
-  print_dht_measurement(&measurement);
-  Serial.println(F("DONE!!"));
+  if (millis() >= next_dht_read) {
+    struct DhtMeasurement measurement = read_dht_measurement();
+    print_dht_measurement(&measurement);
+    next_dht_read = millis() + dht_timeout_ms;
+  }
+
+  handle_client();
 }
