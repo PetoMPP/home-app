af8a9689ee81fd44d5d1a9d1dce1efd2fb2e4b4c
PetoMPP <piotreq22@gmail.com>
2024-09-01 10:44:26 +0200

    sensor: remove location from sensor

--- home-sensor/src/stores/sensor.hpp
+++ home-sensor/src/stores/sensor.hpp
@@ -6,7 +6,6 @@ class SensorStore : public Store
 {
 public:
     char *name = new char[64];
-    char *location = new char[64];
     uint32_t *features = new uint32_t();
     SensorStore() {}
     SensorStore(Preferences *preferences, int max_size, const char *store_name) : Store(preferences, max_size, store_name)
@@ -15,19 +14,12 @@ public:
     void init_json() override
     {
         name[0] = '\0';
-        location[0] = '\0';
         *features = 0;
         const char *n = doc["name"];
         if (n != NULL)
         {
             strcpy(name, n);
         }
-        const char *l = doc["location"];
-        if (l != NULL)
-        {
-            strcpy(location, l);
-        }
-
         if (doc.containsKey("features"))
         {
             *features = doc["features"];
@@ -39,10 +31,6 @@ public:
         if (name != NULL)
         {
             json["name"] = name;
-        }
-        if (location != NULL)
-        {
-            json["location"] = location;
         }
         if (features != NULL)
         {
